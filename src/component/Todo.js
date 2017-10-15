a7df27438c4823c065ee47eba9e95f2648f69012
Nedjim <nedjim.danimon@gmail.com>
2017-10-15 12:45:17 +0200

    Add react and component

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,1 +1,23 @@
 import React from 'react';
+
+export default class Todo extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = { todo: props}
+    }
+
+    render(){
+        console.log(this.state.todo.todo.isDone)
+        if(this.state.todo.todo.isDone){
+            return (
+                <div><strong>{this.state.todo.todo.text}</strong></div>
+            )
+        }
+        else {
+            return (
+                <div> {this.state.todo.todo.text} </div>
+            )
+        }
+    }
+}
--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Todo from './Todo';
 
 const TodoList = (props) => (
 
     <div className='todo'>
         <input type='text' placeholder='Add todo'/>
+        <ul className='todo-list'>
+            {
+                props.todos.map( t => (
+                    <li key={t.id} className='todo-item'>
+                        <Todo todo={t}/>
+                    </li>
+                ))
+            }
+        </ul>
     </div>
 )
 
