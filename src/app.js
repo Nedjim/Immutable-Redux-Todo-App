408cc556ef651db0bd3f17b899ccfcbcb986728f
Nedjim <nedjim.danimon@gmail.com>
2017-10-18 05:52:14 +0200

    Add todo

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
-import TodoList  from './component/TodoList';
-import {List, Map } from 'immutable';
+import {createStore} from 'redux';
+import { Provider } from 'react-redux';
 
-const dummyTodos = List([
-    Map({ id: 0, isDone: true,  text: 'make components' }),
-    Map({ id: 1, isDone: false, text: 'design actions' }),
-    Map({ id: 2, isDone: false, text: 'implement reducer' }),
-    Map({ id: 3, isDone: false, text: 'connect components' })
-  ]);
+import reducer from './reducer/reducer'
+import {TodoList} from './container/container'
 
-render(<TodoList todos={dummyTodos} />, document.getElementById('app'));
+const store = createStore(reducer);
+
+render(<Provider store={store}>
+          <TodoList />
+       </Provider>, document.getElementById('app'));
